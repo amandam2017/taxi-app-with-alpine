c415fb4e4417c41557538c440b0bbfeb26ac9a5a
amandam2017 <amandamaarman19911@gmail.com>
2022-05-12 17:17:19 +0200

    restructured my app.js and added each route in an object array

--- app.js
+++ app.js
@@ -2,22 +2,99 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('taxi', () => ({
         qty: 0,
         leaveCount: 0,
-        availableTaxi:5,
-        totalfair:0,
-        qtyUpFun(){
-            this.qty++
-        },
-        qtydownFun(){
-            this.qty--
-        },
-        leaveCountFun(){
-            this.leaveCount++
-        },
-        actualTaxisAvailable(){
-            return this.availableTaxi - this.leaveCount
-        },
-        calculatedTotalFair(){
-            return this.totalfair+=200
-        }        
-}))
+        availableTaxi: 5,
+        totalfair: 0,
+        passengersPerTaxi: 6,
+        routes: [
+            {
+                name: 'Rondebosch',
+                fare: 15.5,
+                trips: 0,
+                queue: 0,
+                totalFare(){
+                    return Number(this.fare) * 10 // Number(this.passengersPerTaxi)
+                },
+                qtyUpFun() {
+                    this.queue++
+                },
+                qtydownFun() {
+                    this.queue--
+                },
+            },
+            {
+                name: 'Bellville',
+                fare: 23,
+                trips: 0,
+                queue: 0,
+                totalFare(){
+                    return Number(this.fare) * 10 // Number(this.passengersPerTaxi)
+                },
+                qtyUpFun() {
+                    this.queue++;
+                    console.log(this.queue);
+                },
+                qtydownFun() {
+                    this.queue--
+                },
+
+            },
+            {
+                name: 'Langa',
+                fare: 20,
+                trips: 0,
+                queue: 0,
+                totalFare(){
+                    return Number(this.fare) * 10 // Number(this.passengersPerTaxi)
+                },
+                qtyUpFun() {
+                    this.queue++
+                },
+                qtydownFun() {
+                    this.queue--
+                },
+
+            },
+            {
+                name: 'Mfuleni',
+                fare: 22,
+                trips: 0,
+                queue: 0,
+                totalFare(){
+                    return Number(this.fare) * 10 // Number(this.passengersPerTaxi)
+                },
+                qtyUpFun() {
+                    this.queue++
+                },
+                qtydownFun() {
+                    this.queue--
+                },
+
+            }
+        ],
+
+        // qtyUpFun(){
+        //     this.qty++
+        // },
+        // qtydownFun(){
+        //     this.qty--
+        // },
+        // leaveCountFun(){
+        //     this.leaveCount++
+        // },
+        // actualTaxisAvailable(){
+        //     return this.availableTaxi - this.leaveCount
+        // },
+        // calculatedTotalFair(){
+        //     return this.totalfair+=200
+        // },
+        takeTripFromPassengers(currentRoute){
+           
+            currentRoute.trips++;
+            currentRoute.queue -= 10;
+
+            // leaveCountFun(); 
+            // calculatedTotalFair();
+            // return this.qty - this.passengersPerTaxi
+        }
+    }))
 })
