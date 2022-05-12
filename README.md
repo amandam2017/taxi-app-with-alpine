7ba95bb8a6f4b4bca42aaea36fed1d4bec490f32
amandam2017 <amandamaarman19911@gmail.com>
2022-05-12 15:36:22 +0200

    added functionaity for leave button and available taxis including the trips made for the Rondebosch desitation....now I need to work on total per trip

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# taxi-app
 # taxi-app-with-alpine
+
+by default: 
+I have 5 taxis available for each day per route
--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('taxi', () => ({
-        
+        qty: 0,
+        leaveCount: 0,
+        availableTaxi:5,
+        qtyUpFun(){
+            this.qty++
+        },
+        qtydownFun(){
+            this.qty--
+        },
+        leaveCountFun(){
+            this.leaveCount++
+        },
+        actualTaxisAvailable(){
+            return this.availableTaxi - this.leaveCount
+        }        
 }))
 })
